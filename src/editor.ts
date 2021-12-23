e284f52b30ec8bce58dff479de41a78eb734bf82
Lachlan Arthur <git@me.lach.la>
2021-12-24 01:43:05 +1000

    Move colour function out

--- src/editor.ts
+++ src/editor.ts
@@ -1,4 +1,4 @@
-type Channel = 'r' | 'g' | 'b';
+import { hex2rgba } from "./colors";
 
 export default () => ( {
 
@@ -54,47 +54,3 @@ export default () => ( {
   },
 
 } );
-
-function hex2rgba( hex: string ): [ number, number, number, number ] {
-
-  if ( hex.substring( 0, 1 ) === '#' ) {
-    hex = hex.substring( 1 );
-  }
-
-  switch ( hex.length ) {
-
-    default:
-      return [ -1, -1, -1, -1 ];
-
-    case 3:
-      {
-        const [ r, g, b ] = hex;
-        hex = r + r + g + g + b + b + '00';
-      }
-      break;
-
-    case 4:
-      {
-        const [ r, g, b, a ] = hex;
-        hex = r + r + g + g + b + b + a + a;
-      }
-      break;
-
-    case 6:
-      hex += '00';
-      break;
-
-    case 8:
-      // All good
-      break;
-
-  }
-
-  return [
-    parseInt( hex.substr( 0, 2 ), 16 ),
-    parseInt( hex.substr( 2, 2 ), 16 ),
-    parseInt( hex.substr( 4, 2 ), 16 ),
-    parseInt( hex.substr( 6, 2 ), 16 ),
-  ];
-
-}
