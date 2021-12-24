9455e5c63aa8a22213041976f653e9d814c45a87
Lachlan Arthur <git@me.lach.la>
2021-12-24 23:13:37 +1000

    Simplify copy code button

--- src/copyButton.ts
+++ src/copyButton.ts
@@ -4,25 +4,11 @@ export default class CopyButton extends AlpineApp<CopyButton> {
 
   showMessage = false;
 
-  bindings = {
-
-    [ '@clipboard-copy' ]( this: CopyButton ) {
-      this.showMessage = true;
-      setTimeout( () => {
-        this.showMessage = false;
-      }, 1000 );
-    },
-
-    [ ':class' ]( this: CopyButton ) {
-      return {
-        'success': this.showMessage,
-      };
-    },
-
-    [ 'x-text' ]( this: CopyButton ) {
-      return this.showMessage ? 'Copied ✅' : 'Copy code';
-    },
-
+  onCopy() {
+    this.showMessage = true;
+    setTimeout( () => {
+      this.showMessage = false;
+    }, 1000 );
   }
 
 }
